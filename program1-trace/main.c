fe7f2b09a4e69fcdf0f70dd9f322b7101aae0396
Bolt259 <sakul2003@gmail.com>
2025-04-11 19:13:59 -0700

    init main and makefile

---
+++ program1-trace/main.c
@@ -0,0 +1,13 @@
+// main.c
+// Lukas Shipley
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+// ethernet(),
+// icmp(),
+// arp(),
+// ip(),
+// tcp()
