8c8bb179548777fb017014677e7e1f9f4ebfdfcd
Bolt259 <sakul2003@gmail.com>
2025-04-14 13:15:52 -0700

    add changed types to u_ints, working ARP and IP_bad_checksum pcap outs

--- program1-trace/trace.c
+++ program1-trace/trace.c
@@ -10,6 +10,7 @@ void icmp();
 void arp();
 void ip();
 void tcp();
+void udp();
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -28,59 +29,66 @@ int main(int argc, char *argv[]) {
 
     int result;
     struct pcap_pkthdr *header;
-    const unsigned char *packet;
+    const u_int8_t *packet;
 
     int pkt_cnt = 1;
     
     while ((result = pcap_next_ex(handle, &header, &packet)) >= 0) {
-        printf("Packet number: %d  Packet length: %d\n\n", pkt_cnt++, header->len);
+        if (pkt_cnt == 1) {
+            printf("Packet number: %d  Packet Len: %d\n", pkt_cnt++, header->len);
+        } else {
+            printf("\nPacket number: %d  Packet Len: %d\n", pkt_cnt++, header->len);
+        }
 
         // process each packet
 
         ethernet(packet);   // pull ethernet header
 
         // struct ethernet_hdr *eth = (struct ethernet_hdr *)packet; // reintroducing ethernet header struct
-        // u_short eth_type = ntohs(*(unsigned short *)(packet + 12)); // defining eth_type
+        // u_short eth_type = ntohs(*(u_int16_t *)(packet + 12)); // defining eth_type
         
         // determine the type of packet and pull the appropriate header
-        unsigned short eth_type = ntohs(*(unsigned short *)(packet + 12));  // dereference pointer
+        u_int16_t eth_type = ntohs(*(u_int16_t *)(packet + 12));  // dereference pointer
         switch (eth_type) {
-            case ETHER_TYPE_IP:
-                ip(packet);   // pull ip header
-
-                // determine ICMP TCP or UDP
-                int protocol = *(unsigned char *)(packet + 23); // no need to worry abt endianess with single byte operations - dereference pointer
-                
-                // pull header len
-                int header_len = ETHER_HDR_LEN + (packet[14] & 0x0F) * 4; // calculate header length
-
+            case ETHER_TYPE_IP: {
+                int ip_hdr_len;
+                ip_hdr_len = (packet[14] & 0x0F) * 4; // calculate header length
+                ip(packet, ip_hdr_len);   // pull IP header
+        
+                // determine ICMP, TCP, or UDP
+                int protocol = *(u_int8_t *)(packet + 23); // no need to worry about endianness with single-byte operations
+        
+                // pull header length
+                int header_len = ETHER_HDR_LEN + ip_hdr_len; // calculate header length
+        
                 switch (protocol) {
                     case 1:
-                        icmp(packet + header_len);   // pull icmp header
+                        icmp(packet + header_len);   // pull ICMP header
                         break;
                     case 6:
-                        tcp(packet + header_len);   // pull tcp header
+                        tcp(packet + header_len);   // pull TCP header
                         break;
                     case 17:
-                        udp(packet + header_len);   // pull udp header
+                        udp(packet + header_len);   // pull UDP header
                         break;
                     default:
-                        printf("Unknown IP protocol\n");
+                        printf("Unknown protocol\n");
+                        break;
                 }
-
                 break;
+            }
             case ETHER_TYPE_ARP:
-                arp(packet);   // pull arp header
+                arp(packet);   // pull ARP header
                 break;
             default:
                 printf("Unknown packet type\n");
+                break;
         }
-
-        // if (ntohs(*(unsigned short *)(packet + 12)) == ETHER_TYPE_IP) {
+        // if (ntohs(*(u_int16_t *)(packet + 12)) == ETHER_TYPE_IP) {
         //     ip(packet);   // pull ip header
 
         //     // determine ICMP TCP or UDP
-        //     int protocol = *(unsigned char *)(packet + 23); // no need to worry abt endianess with single byte operations - dereference pointer
+        //     int protocol = *(u_int8_t *)(packet + 23); // no need to worry abt endianess with single byte operations - dereference pointer
 
         //     if (protocol == 1) {
         //         icmp(packet);   // pull icmp header
@@ -92,7 +100,7 @@ int main(int argc, char *argv[]) {
         //         printf("Unknown IP protocol\n");
         //     }
         
-        // } else if (ntohs(*(unsigned short *)(packet + 12)) == ETHER_TYPE_ARP) {
+        // } else if (ntohs(*(u_int16_t *)(packet + 12)) == ETHER_TYPE_ARP) {
         //     arp(packet);   // pull arp header
         // } else {
         //     printf("Unknown packet type\n");
@@ -111,24 +119,29 @@ int main(int argc, char *argv[]) {
 
 
 // ethernet header
-void ethernet(const unsigned char *packet) {
+void ethernet(const u_int8_t *packet) {
 
-    unsigned char eth_dest[6]; // destination MAC address
-    unsigned char eth_src[6]; // source MAC address
-    // unsigned short eth_type = ntohs(*(unsigned short *)(packet + 12));
-    unsigned short eth_type;
+    u_int8_t eth_dest[6]; // destination MAC address
+    u_int8_t eth_src[6]; // source MAC address
+    // u_int16_t eth_type = ntohs(*(u_int16_t *)(packet + 12));
+    u_int16_t eth_type;
 
     memcpy(eth_dest, packet, 6);
     memcpy(eth_src, packet + 6, 6);
     memcpy(&eth_type, packet + 12, 2);
     eth_type = ntohs(eth_type);
 
-    printf("\tEthernet Header:\n");
-    printf("\t\tDest MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
-        eth_dest[0], eth_dest[1], eth_dest[2], eth_dest[3], eth_dest[4], eth_dest[5]);
-    printf("\t\tSource MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
-            eth_src[0], eth_src[1], eth_src[2], eth_src[3], eth_src[4], eth_src[5]);
-    printf("\t\tType: %s\n",
+    printf("\n\tEthernet Header\n");
+    printf(
+        "\t\tDest MAC: %x:%x:%x:%x:%x:%x\n",
+        eth_dest[0], eth_dest[1], eth_dest[2], eth_dest[3], eth_dest[4], eth_dest[5]
+    );
+    printf(
+        "\t\tSource MAC: %x:%x:%x:%x:%x:%x\n",
+        eth_src[0], eth_src[1], eth_src[2], eth_src[3], eth_src[4], eth_src[5]
+    );
+    printf(
+        "\t\tType: %s\n",
         (eth_type == ETHER_TYPE_IP) ? "IP" :
         (eth_type == ETHER_TYPE_ARP) ? "ARP" :
         "Unknown"
@@ -136,74 +149,98 @@ void ethernet(const unsigned char *packet) {
 }
 
 // ip header
-void ip(const unsigned char *packet) {
-    printf("\tIP Header\n");
-    printf("\t\tIP PDU Len: %d\n", ntohs(*(unsigned short *)(packet + 16)));
-    printf("\t\tHeader Len (bytes): %d\n", (packet[14] & 0x0F) * 4);
+void ip(const u_int8_t *packet, int ip_hdr_len) {
+    // check if valid IP version
+    // if ((*(u_int8_t *)(packet + 14) & 0xF0) != 0x40) {
+    //     printf("Invalid IP version\n");
+    // }
+    
+    // checksum calcs
+    u_int16_t expected = ntohs(*(u_int16_t *)(packet + 24));
+    u_int16_t actual = in_cksum((u_int16_t *)packet, ip_hdr_len);
+    
+    printf("\n\tIP Header\n");
+    printf("\t\tIP PDU Len: %d\n", ntohs(*(u_int16_t *)(packet + 16)));
+    printf("\t\tHeader Len (bytes): %d\n", ip_hdr_len);
     printf("\t\tTTL: %d\n", packet[22]);
-    printf("\t\tProtocol: %s\n", 
+    printf(
+        "\t\tProtocol: %s\n", 
         (packet[23] == 1) ? "ICMP" : 
         (packet[23] == 6) ? "TCP" : 
         (packet[23] == 17) ? "UDP" : "Unknown"
     );
-
-    unsigned short checksum = ntohs(*(unsigned short *)(packet + 24));
-    
-    printf("\t\tChecksum: %s (0x%04x)\n", 
-        (checksum == 0) ? "Correct" : "Incorrect",
-        checksum
+    printf(
+        "\t\tChecksum: %s (0x%04x)\n", 
+        (actual == 0) ? "Correct" : "Incorrect",    // actual checksum is 0 if valid
+        expected
     );
-    printf("\t\tSender IP: %d.%d.%d.%d\n",
-        packet[26], packet[27], packet[28], packet[29]);
-    printf("\t\tDest IP: %d.%d.%d.%d\n",
-        packet[30], packet[31], packet[32], packet[33]);
+    printf(
+        "\t\tSender IP: %d.%d.%d.%d\n",
+        packet[26], packet[27], packet[28], packet[29]
+    );
+    printf(
+        "\t\tDest IP: %d.%d.%d.%d\n",
+        packet[30], packet[31], packet[32], packet[33]
+    );
 }
 
-// icmp header
-void icmp(const unsigned char *packet) {
-    printf("ICMP Header:\n");
-    printf("Type: %d\n", packet[34]);
-    printf("Code: %d\n", packet[35]);
-    printf("Checksum: 0x%04x\n", ntohs(*(unsigned short *)(packet + 36)));
-    printf("Identifier: %d\n", ntohs(*(unsigned short *)(packet + 38)));
-    printf("Sequence Number: %d\n", ntohs(*(unsigned short *)(packet + 40)));
+// icmp header - check type cause idk
+void icmp(const u_int8_t *packet) {
+    printf("\n\tICMP Header\n");
+    if (packet[0] == 0) {
+        printf("\t\tType: Reply\n");
+    } else if (packet[0] == 8) {
+        printf("\t\tType: Request\n");
+    } else {
+        printf("\t\tType: %d\n", packet[0]);
+    }
 }
+
 // arp header
-void arp(const unsigned char *packet) {
-    printf("ARP Header:\n");
-    printf("Hardware Type: %d\n", ntohs(*(unsigned short *)(packet + 14)));
-    printf("Protocol Type: 0x%04x\n", ntohs(*(unsigned short *)(packet + 16)));
-    printf("Hardware Address Length: %d\n", packet[18]);
-    printf("Protocol Address Length: %d\n", packet[19]);
-    printf("Operation: %d\n", ntohs(*(unsigned short *)(packet + 20)));
-    printf("Sender Hardware Address: %02x:%02x:%02x:%02x:%02x:%02x\n",
-        packet[22], packet[23], packet[24], packet[25], packet[26], packet[27]);
-    printf("Sender Protocol Address: %d.%d.%d.%d\n",
-        packet[28], packet[29], packet[30], packet[31]);
-    printf("Target Hardware Address: %02x:%02x:%02x:%02x:%02x:%02x\n",
-        packet[32], packet[33], packet[34], packet[35], packet[36], packet[37]);
-    printf("Target Protocol Address: %d.%d.%d.%d\n",
-        packet[38], packet[39], packet[40], packet[41]);
+void arp(const u_int8_t *packet) {
+    printf("\n\tARP header\n");
+    printf(
+        "\t\tOpcode: %s\n", 
+        (ntohs(*(u_int16_t *)(packet + 20)) == 1) ? "Request" : 
+        (ntohs(*(u_int16_t *)(packet + 20)) == 2) ? "Reply" : "Unknown"
+    );
+    printf(
+        "\t\tSender MAC: %x:%x:%x:%x:%x:%x\n",
+        packet[22], packet[23], packet[24], packet[25], packet[26], packet[27]
+    );
+    printf(
+        "\t\tSender IP: %d.%d.%d.%d\n",
+        packet[28], packet[29], packet[30], packet[31]
+    );
+    printf(
+        "\t\tTarget MAC: %x:%x:%x:%x:%x:%x\n",
+        packet[32], packet[33], packet[34], packet[35], packet[36], packet[37]
+    );
+    printf(
+        "\t\tTarget IP: %d.%d.%d.%d\n",
+        packet[38], packet[39], packet[40], packet[41]
+    );
 }
+
 // tcp header
-void tcp(const unsigned char *packet) {
-    printf("TCP Header:\n");
-    printf("Source Port: %d\n", ntohs(*(unsigned short *)(packet + 34)));
-    printf("Dest Port: %d\n", ntohs(*(unsigned short *)(packet + 36)));
-    printf("Sequence Number: %u\n", ntohl(*(unsigned int *)(packet + 38)));
-    printf("Acknowledgment Number: %u\n", ntohl(*(unsigned int *)(packet + 42)));
+void tcp(const u_int8_t *packet) {
+    printf("\n\tTCP Header\n");
+    printf("Source Port: %d\n", ntohs(*(u_int16_t *)(packet + 34)));
+    printf("Dest Port: %d\n", ntohs(*(u_int16_t *)(packet + 36)));
+    printf("Sequence Number: %u\n", ntohl(*(u_int32_t *)(packet + 38)));
+    printf("Acknowledgment Number: %u\n", ntohl(*(u_int32_t *)(packet + 42)));
     printf("Data Offset: %d\n", (packet[46] >> 4) & 0x0F);
     printf("Flags: 0x%02x\n", packet[47]);
-    printf("Window Size: %d\n", ntohs(*(unsigned short *)(packet + 48)));
-    printf("Checksum: 0x%04x\n", ntohs(*(unsigned short *)(packet + 50)));
-    printf("Urgent Pointer: %d\n", ntohs(*(unsigned short *)(packet + 52)));
+    printf("Window Size: %d\n", ntohs(*(u_int16_t *)(packet + 48)));
+    printf("Checksum: 0x%04x\n", ntohs(*(u_int16_t *)(packet + 50)));
+    printf("Urgent Pointer: %d\n", ntohs(*(u_int16_t *)(packet + 52)));
 }
 // udp header
-void udp(const unsigned char *packet) {
-    printf("UDP Header:\n");
-    printf("Source Port: %d\n", ntohs(*(unsigned short *)(packet + 34)));
-    printf("Dest Port: %d\n", ntohs(*(unsigned short *)(packet + 36)));
-    printf("Length: %d\n", ntohs(*(unsigned short *)(packet + 38)));
-    printf("Checksum: 0x%04x\n", ntohs(*(unsigned short *)(packet + 40)));
+void udp(const u_int8_t *packet) {
+    printf("\n\tUDP Header\n");
+    printf("Source Port: %d\n", ntohs(*(u_int16_t *)(packet + 34)));
+    printf("Dest Port: %d\n", ntohs(*(u_int16_t *)(packet + 36)));
+    printf("Length: %d\n", ntohs(*(u_int16_t *)(packet + 38)));
+    printf("Checksum: 0x%04x\n", ntohs(*(u_int16_t *)(packet + 40)));
 }
 
--- program1-trace/trace.h
+++ program1-trace/trace.h
@@ -13,33 +13,44 @@
 
 #define ETHER_HDR_LEN 14
 
-// ethernet header structure
-struct ethernet_hdr {
-    unsigned char dest[6]; // destination MAC address
-    unsigned char src[6]; // source MAC address
-    unsigned short ether_type;    // ethernet type
+// // ethernet header structure
+// struct ethernet_hdr {
+//     u_int8_t dest[6]; // destination MAC address
+//     u_int8_t src[6]; // source MAC address
+//     unsigned short ether_type;    // ethernet type
+// };
+
+// // ip header structure
+// struct ip_hdr {
+//     u_int8_t ver_ihl; // version and header length
+//     u_int8_t tos; // type of service
+//     unsigned short total_len; // total length
+//     unsigned short id; // identification
+//     unsigned short frag_off; // fragment offset
+//     u_int8_t ttl; // time to live
+//     u_int8_t proto; // protocol
+//     unsigned short check; // checksum
+//     unsigned int src_ip; // source IP address
+//     unsigned int dest_ip; // destination IP address
+// };
+
+// pseudo header struct for TCP checksum calculation
+struct pseudo_hdr {
+    u_int32_t src_ip_addr;
+    u_int32_t dest_ip_addr;
+    u_int8_t zero;
+    u_int8_t protocol;
+    u_int16_t length;
 };
 
-// ip header structure
-struct ip_hdr {
-    unsigned char ver_ihl; // version and header length
-    unsigned char tos; // type of service
-    unsigned short total_len; // total length
-    unsigned short id; // identification
-    unsigned short frag_off; // fragment offset
-    unsigned char ttl; // time to live
-    unsigned char proto; // protocol
-    unsigned short check; // checksum
-    unsigned int src_ip; // source IP address
-    unsigned int dest_ip; // destination IP address
-};
+
 
 // function declarations
-void ethernet(const unsigned char *packet);
-void icmp(const unsigned char *packet);
-void arp(const unsigned char *packet);
-void ip(const unsigned char *packet);
-void tcp(const unsigned char *packet);
-void udp(const unsigned char *packet);
+void ethernet(const u_int8_t *packet);
+void icmp(const u_int8_t *packet);
+void arp(const u_int8_t *packet);
+void ip(const u_int8_t *packet, int ip_hdr_len);
+void tcp(const u_int8_t *packet);
+void udp(const u_int8_t *packet);
 
 #endif // TRACE_H
